b2ef86cd0bb96e90734038b4b04ab41ff88328c5
Oumayma Saidi <oumaymasaidi908@gmail.com>
2025-05-15 12:45:25 +0100

    budget

---
+++ Backend/src/main/java/org/budgetmanager/backend/Controller/BudgetController.java
@@ -0,0 +1,4 @@
+package org.budgetmanager.backend.Controller;
+
+public interface BudgetController {
+}
---
+++ Backend/src/main/java/org/budgetmanager/backend/Service/BudgetService.java
@@ -0,0 +1,4 @@
+package org.budgetmanager.backend.Service;
+
+public interface BudgetService {
+}
