4e4b6a46f52a1abb5aef1bf70acabf210aed0ad2
AyaAbrada <abradaaya2@gmail.com>
2025-05-15 17:10:53 +0200

    backend transaction

--- Backend/src/main/java/org/budgetmanager/backend/Repository/TransactionRepository.java
+++ Backend/src/main/java/org/budgetmanager/backend/Repository/TransactionRepository.java
@@ -1,4 +1,7 @@
 package org.budgetmanager.backend.Repository;
+import org.budgetmanager.backend.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository {
+public interface TransactionRepository extends JpaRepository<Transaction, Long > {
+
 }
